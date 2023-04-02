4ee88440b42f5cc2b666f308ac1ee015f4d407c6
Manikandan A <116253047+00Maddy@users.noreply.github.com>
2023-04-02 17:29:17 +0530

    2/4/2023

--- Sharpner Dir/Projects/StartingProjets/app.js
+++ Sharpner Dir/Projects/StartingProjets/app.js
@@ -7,14 +7,20 @@ var header = document.getElementById('header-title');
 
 // header.innerHTML = '<h3>Hello</h3>';
 
-var list = document.getElementsByClassName('list-group-item');
-console.log(list);
-list[2].style.backgroundColor = 'green';
-for(i=0;i<list.length;i++){
-    list[i].style.font = 'bold';
-}
+// var list = document.getElementsByClassName('list-group-item');
+// console.log(list);
+// list[2].style.backgroundColor = 'green';
+// for(i=0;i<list.length;i++){
+//     list[i].style.font = 'bold';
+// }
 
 // var last = document.getElementsByClassName('items');
-var last = document.getElementsByClassName('custom-item');
-list[4].style.backgroundColor = 'green';
-console.log(last);
+// var last = document.getElementsByClassName('custom-item');
+// list[4].style.backgroundColor = 'green';
+// console.log(last);
+
+var secondItems = document.querySelector('.list-group-item:nth-child(2)');
+secondItems.style.backgroundColor = 'green';
+
+var thirdItems = document.querySelector('.list-group-item:nth-child(3)');
+thirdItems.style.display = 'none';
