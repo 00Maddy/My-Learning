b7900210162b289203146107c248d89c9edadf66
Manikandan A <116253047+00Maddy@users.noreply.github.com>
2023-04-02 14:20:16 +0530

    2/4/2023

--- Sharpner Dir/Projects/StartingProjets/app.js
+++ Sharpner Dir/Projects/StartingProjets/app.js
@@ -1,1 +1,15 @@
-console.log("I Love to Learning new things");
+// console.log(document.getElementById('main-header'));
+var header = document.getElementById('header-title');
+// console.log(header);
+// header.textContent = 'Hello World'
+// header.innerText = 'dreams';
+// console.log(header.innerText);
+
+// header.innerHTML = '<h3>Hello</h3>';
+
+var list = document.getElementsByClassName('list-group-item');
+console.log(list);
+list[2].style.backgroundColor = 'green';
+for(i=0;i<list.length;i++){
+    list[i].style.font = 'bold';
+}
